ceaa729b780400e611fc7f0e652469fb2eda67c2
Alessio Bollea <alessio.bollea@live.it>
2023-06-12 20:31:15 +0200

    Minor improvements

--- db/models/users.js
+++ db/models/users.js
@@ -1,58 +1,58 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    userId: String,
-    password: String,
-    roles: [String],
-    insertRights: Boolean,
+    userId: {type: String, default: "0"},
+    password: {type: String, default: "*"},
+    roles: {type: [String], default: ["user"]},
+    insertRights: {type: Boolean, default: false},
     session: {
-        sessionId: String,
-        expirationDate: Date
+        sessionId: {type: String, default: "0"},
+        expirationDate: {type: Date, default: new Date(0)}
     }
 });
 
 /* ==================== Template queries ==================== */
 
-userSchema.statics.get = async function(where, select) {
+async function get(where, select) {
     let users = [];
-    const result = await this.find(where, select).exec();
+    const result = await User.find(where, select).exec();
     for (let r of result)
         users.push(r)
     return users;
 }
 
-userSchema.statics.getOne = async function(where, select) {
-    return await this.findOne(where, select);
+async function getOne(where, select) {
+    return await User.findOne(where, select);
 }
 
-userSchema.statics.setOne = async function(where, update) {
-    return await this.findOneAndUpdate(where, {$set: update});
+async function setOne(where, update) {
+    return await User.findOneAndUpdate(where, {$set: update});
 }
 
 /* ==================== Specific queries ==================== */
 
-userSchema.statics.getAllUsersIds = async function() {
-    return await this.get({}, "userId");
+async function getAllUsersIds() {
+    return await get({}, "userId");
 }
 
-userSchema.statics.getPasswordByUserId = async function(user_id) {
-    return await this.getOne({userId: user_id}, "password");
+async function getPasswordByUserId(user_id) {
+    return await getOne({userId: user_id}, "password");
 }
 
-userSchema.statics.setPasswordOfUserId = async function(user_id, hashed_new_pwd) {
-    await this.setOne({userId: user_id}, {password: hashed_new_pwd});
+async function setPasswordOfUserId(user_id, hashed_new_pwd) {
+    await setOne({userId: user_id}, {password: hashed_new_pwd});
 }
 
-userSchema.statics.getRightsByUserId = async function(user_id) {
-    return await this.getOne({userId: user_id}, "insertRights");
+async function getRightsByUserId(user_id) {
+    return await getOne({userId: user_id}, "insertRights");
 }
 
-userSchema.statics.getSessionByUserId = async function(user_id) {
-    return await this.getOne({userId: user_id}, "session");
+async function getSessionByUserId(user_id) {
+    return await getOne({userId: user_id}, "session");
 }
 
-userSchema.statics.setSessionOfUserId = async function(user_id, session_id, expiration_date) {
-    return await this.setOne({userId: user_id}, {session: {
+async function setSessionOfUserId(user_id, session_id, expiration_date) {
+    return await setOne({userId: user_id}, {session: {
         sessionId: session_id,
         expirationDate: expiration_date
     }});
@@ -60,4 +60,12 @@ userSchema.statics.setSessionOfUserId = async function(user_id, session_id, expi
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+module.exports = {
+    User,
+    getAllUsersIds,
+    getPasswordByUserId,
+    setPasswordOfUserId,
+    getRightsByUserId,
+    getSessionByUserId,
+    setSessionOfUserId
+};
