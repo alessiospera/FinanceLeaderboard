34ed79d1de478fcd8126505e74159b7409b6b530
Alessio Speranza <alessiosperanza@outlook.it>
2023-06-18 12:33:10 +0200

    Adjusted visibility of modals

--- src/sections/signUpForm.jsx
+++ src/sections/signUpForm.jsx
@@ -166,16 +166,33 @@ function SignUpPage() {
       cursor: pointer;
     }
 
+    .modal {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        border: 2px solid orange;
+        color: black;
+        text-align: center;
+        padding: 20px;
+    }
+      
     .modal-content {
         background-color: white;
         border: 2px solid orange;
         color: black;
         text-align: center;
         padding: 20px;
-      }
+    }
       
-      .modal-overlay {
+    .modal-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
         background-color: rgba(0, 0, 0, 0.5);
-      }
+    }
       
   `;
