b23296a02fac5fbd339baf61f413e096f95b6fbd
Alessio Bollea <alessio.bollea@live.it>
2023-06-25 16:53:39 +0200

    Added expenses.js to mongo.js exports

--- db/mongo.js
+++ db/mongo.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const users = require("./models/users.js");
 const balances = require("./models/balances.js");
+const expenses = require("./models/expenses.js");
 
 async function connect(uri) {
     return mongoose.connect(uri);
 }
 
-module.exports = {connect, users, balances};
+module.exports = {connect, users, balances, expenses};
